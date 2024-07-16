870af8cdb98a3bbee3e92291a2bdeb4dd810be6a
Pur1ku <167837256+Pur1ku@users.noreply.github.com>
2024-07-16 14:44:27 +0700

    a

--- src/app/components/login.js
+++ src/app/components/login.js
@@ -1,60 +1,34 @@
-import React from 'react';
+// components/LoginForm.js
 import { useForm } from 'react-hook-form';
-<<<<<<< HEAD
 
-import React from 'react'
-
-export default function LoginForm() {
-  return (
-    const LoginForm = () => {
-        const { register, handleSubmit, formState: { errors } } = useForm();
-  )
-}
-
-=======
-import './LoginForm.css'; // Example CSS file for styling
 
 const LoginForm = () => {
->>>>>>> 6696f8de2a10ac925e142ff302200c5a078bd327
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-<<<<<<< HEAD
     <div style={styles.container}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div style={styles.inputContainer}>
-=======
-    <div className="login-form-container">
-      <form >
-        <div className="form-group">
->>>>>>> 6696f8de2a10ac925e142ff302200c5a078bd327
           <label htmlFor="username">Username</label>
           <input
             id="username"
             type="text"
-          
+            {...register('username', { required: true })}
           />
-<<<<<<< HEAD
           {errors.username && <p style={styles.error}>Username is required</p>}
         </div>
         <div style={styles.inputContainer}>
-=======
-         
-        </div>
-        <div className="form-group">
->>>>>>> 6696f8de2a10ac925e142ff302200c5a078bd327
           <label htmlFor="password">Password</label>
           <input
             id="password"
             type="password"
-        
-        
+            {...register('password', { required: true })}
           />
-<<<<<<< HEAD
           {errors.password && <p style={styles.error}>Password is required</p>}
-=======
-         
->>>>>>> 6696f8de2a10ac925e142ff302200c5a078bd327
         </div>
         <button type="submit">Login</button>
       </form>
