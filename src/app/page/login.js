86f537897ccd3ef2219855fd446818a6ef570c59
FEW <pfewddot@gmail.com>
2024-07-16 14:15:58 +0700

    update

--- src/app/page/login.js
+++ src/app/page/login.js
@@ -3,9 +3,9 @@ import LoginForm from '../components/login';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Login Page</h1>
+    <div >
+      <main >
+        <h1 >Login Page</h1>
         <LoginForm />
       </main>
     </div>
